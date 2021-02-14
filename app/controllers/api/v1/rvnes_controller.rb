daa38a90d74293102155a99ac195d6a96841607f
RKRedfern <rkredfern@gmail.com>
2021-02-14 16:31:49 -0500

    rvne create route and controller action added with error functionality

--- app/controllers/api/v1/rvnes_controller.rb
+++ app/controllers/api/v1/rvnes_controller.rb
@@ -1,3 +1,22 @@
 class Api::V1::RvnesController < ApplicationController
-    
+
+    def index
+        rvnes = Rvne.all
+        render json: rvnes
+    end
+
+    def create 
+        rvne = Rvne.new(rvne_params)
+        if rvne.save
+            render json: rvne, status: :accepted
+        else 
+            render json: {errors: rvne.errors.full_messages}, status: :unprocessible_entity
+    end
+
+
+    private 
+
+    def rvne_params
+        params.require(:rvne).permit(:content, :user_id)
+    end
 end
