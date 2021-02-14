4347fa4cd581d77314c0aa8e4333de41daeb2cd1
RKRedfern <rkredfern@gmail.com>
2021-02-14 16:01:07 -0500

    rvne index and controller g

---
+++ app/controllers/api/v1/rvnes_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::RvnesController < ApplicationController
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :rvnes
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :api do 
+    namespace :v1 do
+      resources :rvnes, only: [:index]
+    end
+  end
+
 end
