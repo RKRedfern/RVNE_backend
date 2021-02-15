48ecc04d8e9c8183ca37818fc9498f1c44653a3e
RKRedfern <rkredfern@gmail.com>
2021-02-15 10:07:49 -0500

    user controller/routes index/create action

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :username, :rvne
+  attributes :username, :rvnes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do 
+    namespace :v1 do
+      resources :users, only: [:index, :create]
+    end
+  end
+
 end
